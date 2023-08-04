653f4ed204a34fafe6c7078af730c31dd7107c25
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2023-08-04 17:49:15 -0300

    termino painel e modal de visualização

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes,
-  Outlet,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ItemPanel from "./components/ItemPanel";
 import ReadItems from "./components/ReadItems";
 import Modal from "react-modal";
+import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -32,6 +27,15 @@ function App() {
     { item: "Item 1", kl: 58.5 },
     { item: "Item 2", kl: 81.2 },
     { item: "Item 3", kl: 67.4 },
+    { item: "Item 4", kl: 80.8 },
+    { item: "Item 5", kl: 115.0 },
+    { item: "Item 6", kl: 76.2 },
+    { item: "Item 7", kl: 92.4 },
+    { item: "Item 8", kl: 68.8 },
+    { item: "Item 9", kl: 65.9 },
+    { item: "Item 10", kl: 50.0 },
+    { item: "Item 11", kl: 50.4 },
+    { item: "Item 12", kl: 274.4 },
   ];
 
   return (
@@ -41,9 +45,15 @@ function App() {
         <table className='table table-striped'>
           <thead>
             <tr>
-              <th>Item</th>
-              <th>Kg</th>
-              <th>Ver</th>
+              <th>
+                <strong>Item</strong>
+              </th>
+              <th>
+                <strong>Kg</strong>
+              </th>
+              <th>
+                <strong>Ação</strong>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -56,7 +66,7 @@ function App() {
                     className='btn btn-primary'
                     onClick={() => handleItemDescription(data)}
                   >
-                    Ver
+                    <i className='bi bi-eye'></i>
                   </button>
                 </td>
               </tr>
@@ -73,59 +83,59 @@ function App() {
         isOpen={isModalOpen}
         onRequestClose={handleModalClose}
         contentLabel='Descrição do Item'
-        className='modal-dialog modal-dialog-centered'
+        style={{
+          content: {
+            maxWidth: "400px",
+            maxHeight: "300px", // Defina a altura máxima como 80% da altura da viewport
+            margin: "auto", // Para centralizar horizontalmente
+          },
+          overlay: { backgroundColor: "rgba(0, 0, 0, 0.6)" },
+        }}
       >
-
-          <div class='modal-dialog'>
-            <div className='modal-content'>
-              <div className='modal-header'>
-                <h1 className='modal-title fs-5'>
-                  Descrever Uma Observação do Item
-                </h1>
-                <button
-                  type='button'
-                  className='btn-close'
-                  onClick={handleModalClose}
-                ></button>
-              </div>
-              <div className='modal-body'>
-                <p>
-                  <strong>Item:</strong> {itemDescription.item}
-                </p>
-                <p>
-                  <strong>Kg:</strong> {itemDescription.kl}
-                </p>
-                <textarea
-                  className='form-control'
-                  value={itemDescription.description || ""}
-                  onChange={(e) =>
-                    setItemDescription({
-                      ...itemDescription,
-                      description: e.target.value,
-                    })
-                  }
-                />
-              </div>
-              <div className='modal-footer'>
-                <button
-                  className='btn btn-primary'
-                  onClick={() => {
-                    handleAddItem(itemDescription);
-                    handleModalClose();
-                  }}
-                >
-                  Enviar para Lidos
-                </button>
-                <button
-                  className='btn btn-secondary'
-                  onClick={handleModalClose}
-                >
-                  Fechar
-                </button>
-              </div>
-            </div>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h1 className='modal-title fs-5'>
+              Descrever Uma Observação do Item
+            </h1>
+            <button
+              type='button'
+              className='btn-close'
+              onClick={handleModalClose}
+            ></button>
           </div>
-
+          <div className='modal-body'>
+            <p>
+              <strong>Item:</strong> {itemDescription.item}
+            </p>
+            <p>
+              <strong>Kg:</strong> {itemDescription.kl}
+            </p>
+            <textarea
+              className='form-control'
+              value={itemDescription.description || ""}
+              onChange={(e) =>
+                setItemDescription({
+                  ...itemDescription,
+                  description: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className='modal-footer my-2'>
+            <button
+              className='btn btn-primary me-2'
+              onClick={() => {
+                handleAddItem(itemDescription);
+                handleModalClose();
+              }}
+            >
+              Enviar para Lidos
+            </button>
+            <button className='btn btn-danger' onClick={handleModalClose}>
+              Fechar
+            </button>
+          </div>
+        </div>
       </Modal>
     </Router>
   );
